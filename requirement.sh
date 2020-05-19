0b70305df429efbe46d866ecf43c6e2671249ce4
shahanialtafhussain <64235650+shahanialtafhussain@users.noreply.github.com>
2020-05-20 00:36:50 +0300

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,1 @@
+#!/bin/bash clearapt updateapt upgradeapt install figlet -yfiglet Basic Installationapt install toilet -yapt install cowsay -yapt install nano -yapt install ruby -ygem install lolcatfiglet -f big Done !!! | lolcatechoecho -e "\e[1m Now Run \e[32mbash t-ban.sh\e[0m...!!!"echoecho -e "\e[1m\e[32m Developed by :\e[33m Sutariya Parixit (8h4i)"
